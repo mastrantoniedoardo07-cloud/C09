5f3ba68e034d464ec627281a0d7e9ed2e2b0cdf1
Edoardo Mastrantoni <emastran@e3r5p5.42roma.it>
2026-02-04 12:25:19 +0100

    agg

--- ex02/ft_split.c
+++ ex02/ft_split.c
@@ -6,39 +6,112 @@
 /*   By: emastran <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/03 13:02:50 by emastran          #+#    #+#             */
-/*   Updated: 2026/02/03 13:25:05 by emastran         ###   ########.fr       */
+/*   Updated: 2026/02/04 12:24:41 by emastran         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-int check(char *str, char *charset)
+#include <stdlib.h>
+#include <stdio.h>
+
+int	check(char c, char *charset)
 {
-	int i;
+	int	i;
 
 	i = 0;
-	while(*charset)
+	while (charset[i])
 	{
-		if(str[i] == charset[i])
+		if (c == charset[i])
 			return (1);
 		i++;
 	}
 	return (0);
 }
-int count(char *str)
+
+int	count_substrings(char *str, char *charset)
 {
 	int	i;
 	int	count;
 
-	while(str[i])
+	i = 0;
+	count = 0;
+	while (str[i])
 	{
-		while(str[i] && check(str[i], charset))
+		while (str[i] && check(str[i], charset))
 			i++;
 		if (str[i] && !check(str[i], charset))
+		{
 			count++;
-		while (str[i] && !check(str[i], charset))
-			i++;
+			while (str[i] && !check(str[i], charset))
+				i++;
+		}
 	}
 	return (count);
 }
-char **ft_split(char *str, char *charset)
+
+char	*copy_substring(char *str, int start, int len)
 {
-	
+	char	*sub;
+	int		k;
+
+	sub = (char *)malloc(sizeof(char) * (len + 1));
+	if (!sub)
+		return (NULL);
+	k = 0;
+	while (k < len)
+	{
+		sub[k] = str[start + k];
+		k++;
+	}
+	sub[k] = '\0';
+	return (sub);
 }
+
+char	**ft_split(char *str, char *charset)
+{
+	char	**result;
+	int		i;
+	int		j;
+	int		len;
+
+	result = (char **)malloc(sizeof(char *) * (count_substrings(str, charset) + 1));
+	if (!result)
+		return (NULL);
+	i = 0;
+	j = 0;
+	while (str[i])
+	{
+		while (str[i] && check(str[i], charset))
+			i++;
+		if (str[i] && !check(str[i], charset))
+		{
+			len = 0;
+			while (str[i + len] && !check(str[i + len], charset))
+				len++;
+			result[j] = copy_substring(str, i, len);
+			if (!result[j])
+				return (NULL);
+			j++;
+			i += len;
+		}
+	}
+	result[j] = NULL;
+	return (result);
+}
+
+/*int	main(void)
+{
+	char	*str;
+	char	*charset;
+	char	**res;
+	int		i;
+
+	str = "ciao,come.va?oggi";
+	charset = ",.?";
+	res = ft_split(str, charset);
+	i = 0;
+	while (res[i])
+	{
+		printf("[%s]\n", res[i]);
+		i++;
+	}
+	return (0);
+}*/
